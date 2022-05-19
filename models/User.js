bde858f3bcb74731e9c68713f2d7f9da031b8f47
Jenna Saikaly <jen.saikaly@gmail.com>
2022-05-19 13:45:56 -0400

    use hooks to hash the password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 //imports model class and DataTypes object from Sequelize
 
 const { Model, DataTypes } = require('sequelize');
@@ -51,7 +52,20 @@ User.init(
 },
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+      
+      hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        //10 = saltRound value
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+         // set up beforeUpdate lifecycle "hook" functionality
+  async beforeUpdate(updatedUserData) {
+    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+    return updatedUserData;
+  }
+      },
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
