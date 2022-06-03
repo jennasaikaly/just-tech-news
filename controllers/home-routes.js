e86f13eb3520fdc73454b969cc35179bf3c86239
jennasaikaly <jen.saikaly@gmail.com>
2022-06-03 14:31:15 -0400

    created single-post view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,7 +32,10 @@ router.get('/', (req, res) => {
           //version of itself, saving the results in a new 'posts' array
         const posts = dbPostData.map(post => post.get({ plain: true }));
         // pass a single post object into the homepage template
-        res.render('homepage', { posts });
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
       })
       .catch(err => {
         console.log(err);
@@ -85,7 +88,10 @@ router.get('/', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
       })
       .catch(err => {
         console.log(err);
