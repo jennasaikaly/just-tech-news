83edea902fb283910ad2e06e4f1c458cf780697c
jennasaikaly <jen.saikaly@gmail.com>
2022-06-04 17:16:21 -0400

    added edit page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote, Comment } = require("../../models");
+const { User, Post, Comment, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -23,16 +23,16 @@ router.get('/:id', (req, res) => {
     },
     include: [
       {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
         model: Comment,
         attributes: ['id', 'comment_text','created_at'],
         include: {
           model: Post,
           attributes: ['title']
         }
-      },
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
       },
       {
         model: Post,
